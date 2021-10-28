4a74db170462cee6d3e0c195cee46fd774a05d01
ShamyakGoel <93246695+ShamyakGoel@users.noreply.github.com>
2021-10-28 13:44:08 +0530

    Bug fixes

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
   install_requires=[],
   long_description_content_type="text/x-rst",
   classifiers=[
-    'Development Status :: 5 - Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 5 - Production',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license 
