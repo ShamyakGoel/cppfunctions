faec9f3f2692548f75cc28f205ae31202054a05c
ShamyakGoel <93246695+ShamyakGoel@users.noreply.github.com>
2021-10-28 12:09:04 +0530

    bug fixes and added two new functions

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(
-  name = 'cppfunctions',         # How you named your package folder (MyLib)
+  name = 'cppfunctions',         # How you named your package folder (cppfunctions)
   packages = ['cppfunctions'],   # Chose the same as "name"
   version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
