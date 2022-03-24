2cf06ca9090490dd9fdfc5db970173218af0b8e3
ShamyakGoel <sj907822@gmail.com>
2022-03-24 14:08:50 +0530

    New version of cppfunctions, 2.1 !
    New functions:
    strlen: Identical tto c/c++ strlen function. Returns length of string.
    strcmp: Identical tto c/c++ strcmp function. Returns true if string is equal to another string else false.
    strncmp: Identical tto c/c++ strncmp function. Returns true if string is not equal to another string else false.
    strlwr: Identical tto c/c++ strlwr function. Returns lowercase version of string.
    strupr: Identical tto c/c++ strupr function. Returns uppercase version of string.
    strrev: Identical tto c/c++ strrev function. Returns reverse of string.
    strdup: Identical tto c/c++ strdup function. Returns duplicate of string.
    strchr: Identical tto c/c++ strchr function. Return the lowest index in string where character is found else false.
    strstr: Identical tto c/c++ strstr function. Return the lowest index in string where substring is found else false.

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from distutils.core import setup
 setup(
-  name = 'cppfunctions',         # How you named your package folder (cppfunctions)
-  packages = ['cppfunctions'],   # Chose the same as "name"
-  version = '2.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'My attempt to bring c/c++ functions to python',   # Give a short description about your library
-  author = 'Shamyak',                   # Type in your name
-  author_email = 'sj907822@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/ShamyakGoel/cppfunctions/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ShamyakGoel/cppfunctions/archive/refs/tags/v_21.tar.gz',    # I explain this later on
-  keywords = ['c++functions', 'cfunctions'],   # Keywords that define your package best
+  name = 'cppfunctions',
+  packages = ['cppfunctions'],
+  version = '2.1', 
+  license='MIT',  
+  description = 'My attempt to bring c/c++ functions to python',
+  author = 'Shamyak',
+  author_email = 'sj907822@gmail.com', 
+  url = 'https://github.com/ShamyakGoel/cppfunctions/',
+  download_url = 'https://github.com/ShamyakGoel/cppfunctions/archive/refs/tags/v_21.tar.gz',  
+  keywords = ['c++functions', 'cfunctions'],
   install_requires=[],
   long_description_content_type="text/x-rst",
   classifiers=[
-    'Development Status :: 5 - Production',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers', 
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license 
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
