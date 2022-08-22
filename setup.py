372693d420648bbd3d9a43e3224afee6c18a8e4a
ShamyakGoel <sj907822@gmail.com>
2022-08-22 16:46:59 +0530

    Some other bug fixes

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
   install_requires=[],
   long_description_content_type="text/x-rst",
   classifiers=[
-    'Development Status :: 5 - Production', 
+    'Development Status :: 5 - Production/Stable', 
     'Intended Audience :: Developers', 
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
